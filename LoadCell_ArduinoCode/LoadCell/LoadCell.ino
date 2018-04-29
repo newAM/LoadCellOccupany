dddf5563945464b3fb1ba4a58d5f56d8c5c1fa16
Alex M <7845120+newAM@users.noreply.github.com>
2018-04-29 11:41:56 -0700

    added tare functionality and serial prints for debug

--- LoadCell_ArduinoCode/LoadCell/LoadCell.ino
+++ LoadCell_ArduinoCode/LoadCell/LoadCell.ino
@@ -3,85 +3,190 @@
 #include <PubSubClient.h>
 #include "HX711.h"
 
-#define DOUT 5 // HX711 DOUT pin
-#define CLK  6 // HX711 CLK pin
-
-// LSB to Kg calibration factor
-#define CALIBRATION -22990
+// HX711 pin connection
+#define DOUT 5
+#define CLK  6
 
 // HX711 sample settings
-#define NUM_SAMPLES   3     // number of sales to take per data point
-#define SAMPLE_PERIOD 100   // sample period in ms
+#define CALIBRATION   -22990 // ADC bits to Kg conversion factor
+#define NUM_SAMPLES   3      // number of sales to take per data point
+#define SAMPLE_PERIOD 100    // sample period in ms
+#define RESULT_WIDTH  5      // number of characters in result
+#define RESOLUTION    1      // number of characters after the decimal
 
-// network settings
-byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
-IPAddress server(10, 0, 0, 4);
+// connection settings
+#define TIMEOUT   5000 // connection failure timeout in ms
+#define SER_BAUD  9600 // serial baud rate
+#define MQTT_PORT 1883 // MQTT broker port
+#define MQTT_CLIENT_ID "bedCell" // device client ID
 
-// sample timer
-unsigned long timer;
+// MQTT topics
+#define TOPIC_BASE "/home/bedroom/bed" // base topic
+#define TOPIC_LOAD TOPIC_BASE "/load"  // topic to publish samples on
+#define TOPIC_TARE TOPIC_BASE "/tare"  // topic to recieve tare commands on
 
-// HX711
+// client objects
+EthernetClient ethClient;
+PubSubClient mqttClient(ethClient);
+
+// HX711 load cell ADC
 HX711 scale(DOUT, CLK);
 
-// ethernet client
-EthernetClient ethClient;
+// reconnects to the MQTT broker
+void reconnect()
+{
+    // loop while disconnected
+    while (!mqttClient.connected())
+    {
+        // attempt to connect
+        Serial.println("Attempting to establish connection to MQTT broker...");
+        if (mqttClient.connect(MQTT_CLIENT_ID)) 
+        {
+            // print connection messaged for debug
+            Serial.println("Connection established");
+            mqttClient.publish(TOPIC_BASE, "connected");
 
-// mqtt client
-PubSubClient client(ethClient);
+            // subscribe to control topic
+            mqttClient.subscribe(TOPIC_TARE);
+        }
+        else
+        {
+            Serial.print("Failed to connect: ");
 
-// 
-int samples = 0;
-float avg = 0;
+            // print client state
+            switch (mqttClient.state())
+            {
+                case MQTT_CONNECTION_TIMEOUT:
+                    Serial.println("MQTT_CONNECTION_TIMEOUT");
+                    break;
+                case MQTT_CONNECTION_LOST:
+                    Serial.println("MQTT_CONNECTION_LOST");
+                    break;
+                case MQTT_CONNECT_FAILED:
+                    Serial.println("MQTT_CONNECT_FAILED");
+                    break;
+                case MQTT_DISCONNECTED:
+                    Serial.println("MQTT_DISCONNECTED");
+                    break;           
+                case MQTT_CONNECTED:
+                    Serial.println("MQTT_CONNECTED");
+                    break;
+                case MQTT_CONNECT_BAD_PROTOCOL:
+                    Serial.println("MQTT_CONNECT_BAD_PROTOCOL");
+                    break;
+                case MQTT_CONNECT_BAD_CLIENT_ID:
+                    Serial.println("MQTT_CONNECT_BAD_CLIENT_ID");
+                    break;
+                case MQTT_CONNECT_UNAVAILABLE:
+                    Serial.println("MQTT_CONNECT_UNAVAILABLE");
+                    break;
+                case MQTT_CONNECT_BAD_CREDENTIALS:
+                    Serial.println("MQTT_CONNECT_BAD_CREDENTIALS");
+                    break;
+                case MQTT_CONNECT_UNAUTHORIZED:
+                    Serial.println("MQTT_CONNECT_UNAUTHORIZED");
+                    break;
+                default:
+                    Serial.println("MQTT_STATE_UNKNOWN");
+                    break;
+            }
+
+            // delay before next attempt
+            delay(TIMEOUT);
+        } 
+    }
+}
+
+// callback function for handling new messages on subscribed topics
+void callback(char* topic, byte* payload, unsigned int length)
+{
+    // zero out scale offset
+    if (!strcmp(topic, TOPIC_TARE))
+    {
+        Serial.println("Zeroing...");
+        scale.tare();
+    }
+}
 
 void setup()
 {
-    // connect with dhcp
+    // network settings
+    const uint8_t mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02}; // device MAC
+    IPAddress mqttBroker(10, 0, 0, 4); // MQTT broker IP
+
+    // open serial connection
+    Serial.begin(SER_BAUD);
+
+    // print status
+    Serial.println("Serial connected!");
+    Serial.println("Configuring Ethernet with DHCP...");
+
+    // connect with DHCP
     while (!Ethernet.begin(mac))
     {
-        delay(1000);
+        delay(TIMEOUT);
     }
-    client.setServer(server, 1883);
 
+    // print IP address
+    Serial.println("Ethernet connected!");
+    Serial.print("My IP: ");
+    Serial.println(Ethernet.localIP());
+
+    // configure MQTT broker
+    mqttClient.setServer(mqttBroker, MQTT_PORT);
+
+    // set callback function
+    mqttClient.setCallback(callback);
+
+    // set scale calibration
     scale.set_scale(CALIBRATION);
-    scale.tare();
 
-    // allow the hardware to sort itself out
-    delay(1500);
+    // zero out scale
+    scale.tare();
 }
 
 void loop()
 {
-    char result[8];
+    static uint32_t timer   = 0; // sample timer
+    static uint8_t  samples = 0; // number of samples taken
+    static float    avg     = 0; // load average
 
-    client.loop();
-
-    // loop until connection established
-    while (!client.connected()) 
+    // connect if disconnected
+    if (!mqttClient.connected())
     {
-        // attempt to connect
-        if (client.connect("bedLoad")) 
-        {
-            client.publish("/home/bedroom/bed", "init");
-        }
-        else
-        {
-            delay(5000);
-        }
+        reconnect();
     }
 
-    // sample every 100ms
+    // take sample
     if (millis() - timer >= SAMPLE_PERIOD)
     {
+        // take a sample
         avg += scale.get_units();
         samples++;
+
         if (samples >= NUM_SAMPLES)
-        { 
+        {
+            // buffer for the sample as a string
+            uint8_t result[RESULT_WIDTH + 1]; 
+
+            // average the samples
             avg /= samples;
-            dtostrf(avg, 4, 1, result);
-            client.publish("/home/bedroom/bed/load", result);
-            timer = millis();
-            avg = 0;
+
+            // convert the float to a string
+            dtostrf(avg, RESULT_WIDTH, RESOLUTION, result);
+
+            // publish string
+            mqttClient.publish(TOPIC_LOAD, result);
+            
+            // clear variables
+            avg     = 0;
             samples = 0;
         }
+
+        // reset timer
+        timer = millis();
     }
+
+    // maintain connection
+    mqttClient.loop();
 }
